160920657fbdbee377082cd67188760553494d7d
Emily Gleeson <engleeson@gmail.com>
2021-06-13 15:21:16 -0400

    more work on questions + generatemarkdown.js

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,10 @@
 // TODO: Include packages needed for this application
 const inquirer = require("inquirer");
 const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown");
 // TODO: Create an array of questions for user input
-const questions =
 
-  inquirer
-    .prompt ([
-        //questions, [
-  {  
+const questions = [{  
   type: "input",
   message:"What is the title of your project?",
   name: "title",
@@ -59,18 +56,29 @@ const questions =
       type: "input",
       message: "What is your email address?",
       name: "email",
-  }
+ 
+},
 ]
-    ,)
-    .then ((response) => {
-        console.log(response);
-    }
-    )
-// // TODO: Create a function to write README file
-// function writeToFile(fileName, data) {}
+   
+
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+    fileName = `${data.title.split(" ").join("")}.md`
+    fs.writeFile(fileName, generateMarkdown)
+;}
+
 
 // // TODO: Create a function to initialize app
-// function init() {}
+    function init() {
+        inquirer.prompt(questions)
+        .then ((response) => {
+        console.log(response);
+        let data = response
+        console.log(data)
+       }, )
+
+    }
 
 // // Function call to initialize app
-// init();
+ init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,20 +1,98 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+switch (license){
+
+  case "Apache License 2.0":
+    badge = `[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)]`;
+    break;
+  case "GNU General Public License v3.0":
+    badge = `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)]`;
+    break;
+  case "MIT License":
+    badge = `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)]`;
+    break;
+  case  "BSD 2-Clause Simplified License":
+    badge = `[![License](https://img.shields.io/badge/License-BSD%202--Clause-orange.svg)]`;
+    break;
+  case "BSD 3-Clause New or Revisex License":
+    badge = `[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)]`;
+    break;
+  case "Eclipse Public License v2.0":
+    badge = `[![License](https://img.shields.io/badge/License-EPL%201.0-red.svg)]`;
+    break;
+  case "GNU General Public License v2.0":
+    badge = `[![License: GPL v2](https://img.shields.io/badge/License-GPL%20v2-blue.svg)]`;
+    break;
+  case "Mozilla Public License 2.0":
+    badge = `[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)]`;
+  default:
+    badge = " "
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  switch(license) {
+    case "Apache License 2.0":
+    link = `[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)]`;
+    break;
+  case "GNU General Public License v3.0":
+    link = `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)]`;
+    break;
+  case "MIT License":
+    link = `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)]`;
+    break;
+  case  "BSD 2-Clause Simplified License":
+    link = `[![License](https://img.shields.io/badge/License-BSD%202--Clause-orange.svg)]`;
+    break;
+  case "BSD 3-Clause New or Revisex License":
+    link = `[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)]`;
+    break;
+  case "Eclipse Public License v2.0":
+    link = `[![License](https://img.shields.io/badge/License-EPL%201.0-red.svg)]`;
+    break;
+  case "GNU General Public License v2.0":
+    link = `[![License: GPL v2](https://img.shields.io/badge/License-GPL%20v2-blue.svg)]`;
+    break;
+  case "Mozilla Public License 2.0":
+    link = `[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)]`;
+  default:
+    link = " "
+  }
+
+}
+
+
+
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {}
 
+
+
+
+
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
-
-`;
+  return `# ${data.title}` +
+    renderLicenseBadge +
+  `#Description
+  ${data.description}
+  #Installation Instructions
+  ${data.installation}
+  #Usage Information
+  ${data.usage}
+  #Contribution Guidelines
+  ${data.contribution}
+  #Testing Instructions
+  ${data.testing}
+  #License
+  ${data.license}
+  #Questions?
+  Find me on github - ${data.github} or send me an email at ${data.email}.`;
 }
 
 module.exports = generateMarkdown;
